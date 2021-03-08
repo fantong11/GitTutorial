82368be1b494492a19bea336040401109bd9ea1f
d89012255 <d89012255@gmail.com>
2021-03-09 00:31:38 +0800

    tutorial1

---
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include <string>
+#include <iostream>
+using namespace System;
+bool Guess(int number) {
+	return 0;
+}
+int main(cli::array<System::String ^> ^args)
+{
+	return 0;
+}
