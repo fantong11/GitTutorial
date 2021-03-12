4355c0e89a5acb2f055548506846fe4568c64ebb
d89012255 <d89012255@gmail.com>
2021-03-12 11:29:20 +0800

    no message

--- game4.10/Source/mygame.cpp
+++ game4.10/Source/mygame.cpp
@@ -95,14 +95,14 @@ void CGameStateInit::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
 	const char KEY_ESC = 27;
 	const char KEY_SPACE = ' ';
 	if (nChar == KEY_SPACE)
-		GotoGameState(GAME_STATE_RUN);						// 切換至GAME_STATE_RUN
+		GotoGameState(GAME_STATE_CHOOSE);						// 切換至GAME_STATE_RUN
 	else if (nChar == KEY_ESC)								// Demo 關閉遊戲的方法
 		PostMessage(AfxGetMainWnd()->m_hWnd, WM_CLOSE,0,0);	// 關閉遊戲
 }
 
 void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
 {
-	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
+	GotoGameState(GAME_STATE_CHOOSE);		// 切換至GAME_STATE_RUN
 }
 
 void CGameStateInit::OnShow()
@@ -388,4 +388,34 @@ void CGameStateRun::OnShow()
 	corner.SetTopLeft(SIZE_X-corner.Width(), SIZE_Y-corner.Height());
 	corner.ShowBitmap();
 }
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的選擇狀態(Game Choose)
+/////////////////////////////////////////////////////////////////////////////
+
+CGameStateChoose::CGameStateChoose(CGame *g)
+	: CGameState(g)
+{
 }
+
+
+void CGameStateChoose::OnInit()
+{
+	ShowInitProgress(0);	// 一開始的loading進度為0%
+	//
+	// 開始載入資料
+	//
+	CHO.LoadBitmap(IDB_CHOOSE);
+	//Sleep(300);				// 放慢，以便看清楚進度，實際遊戲請刪除此Sleep
+	//
+	// 此OnInit動作會接到CGameStaterRun::OnInit()，所以進度還沒到100%
+	//
+}
+
+void CGameStateChoose::OnShow()
+{
+	CHO.SetTopLeft(0, 0);
+	CHO.ShowBitmap();
+}
+
+}
+
