8b227a12c6649e73ed3c8a60edef48e5f366b46c
d89012255 <d89012255@gmail.com>
2021-03-12 10:30:01 +0800

    load try

--- game4.10/Source/resource.h
+++ game4.10/Source/resource.h
@@ -35,9 +35,10 @@
 #define IDB_HELP                        155
 #define IDB_BITMAP2                     156
 #define IDB_LOADING                     156
-#define IDB_INIBG                       159
 #define IDB_BITMAP3                     161
-#define IDB_LOADAD                      161
+#define IDB_INIBG                       162
+#define IDB_BITMAP4                     163
+#define IDB_LOADAD                      163
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -50,7 +51,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        162
+#define _APS_NEXT_RESOURCE_VALUE        164
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
